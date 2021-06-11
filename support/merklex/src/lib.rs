16b36ed932788577c7017b4b6bac890ea8e4ff44
Shelby Doolittle <shelby@shelbyd.com>
2021-06-11 11:09:58 -0400

    add push method to merkle tree

--- support/merklex/src/lib.rs
+++ support/merklex/src/lib.rs
@@ -7,7 +7,7 @@ use sp_std::{
     prelude::{Box, Vec},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct MerkleTree<D: Digest> {
     hash: GenericArray<u8, D::OutputSize>,
     children: Option<(Box<Self>, Box<Self>)>,
@@ -21,6 +21,13 @@ impl<D: Digest> MerkleTree<D> {
     pub fn leaf(hash: GenericArray<u8, D::OutputSize>) -> Self {
         MerkleTree {
             hash,
+            children: None,
+        }
+    }
+
+    pub fn leaf_bytes(bytes: &[u8]) -> Self {
+        MerkleTree {
+            hash: D::digest(bytes),
             children: None,
         }
     }
@@ -40,6 +47,30 @@ impl<D: Digest> MerkleTree<D> {
         self.children
             .as_ref()
             .map(|(l, r)| (l.as_ref(), r.as_ref()))
+    }
+
+    pub fn push(self, value: impl AsRef<[u8]>) -> Self {
+        let new_leaf = Self::leaf_bytes(value.as_ref());
+
+        match self.children {
+            _ if self.balanced() => Self::merge(self, new_leaf),
+            Some((left, right)) => Self::merge(*left, right.push(value)),
+            None => unreachable!(),
+        }
+    }
+
+    pub fn weight(&self) -> usize {
+        self.children
+            .as_ref()
+            .map(|(l, r)| l.weight() + r.weight() + 1)
+            .unwrap_or(1)
+    }
+
+    pub fn balanced(&self) -> bool {
+        self.children
+            .as_ref()
+            .map(|(l, r)| l.weight() == r.weight())
+            .unwrap_or(true)
     }
 }
 
@@ -63,7 +94,7 @@ pub fn build_tree<D: Digest, T: AsRef<[u8]>, I: IntoIterator<Item = T>>(
 ) -> Option<MerkleTree<D>> {
     let leaves = items
         .into_iter()
-        .map(|item| MerkleTree::leaf(D::digest(item.as_ref())))
+        .map(|item| MerkleTree::leaf_bytes(item.as_ref()))
         .collect::<VecDeque<_>>();
     build_from_layer::<D>(leaves)
 }
@@ -99,18 +130,18 @@ mod tests {
     use blake2::Blake2b;
     use hex_literal::hex;
 
-    #[cfg(test)]
-    mod build_tree_ {
-        use super::*;
-
-        fn hello_world_hash() -> [u8; 64] {
-            hex!(
-                "
-                021ced8799296ceca557832ab941a50b4a11f83478cf141f51f933f653ab9fbc
-                c05a037cddbed06e309bf334942c4e58cdf1a46e237911ccd7fcf9787cbc7fd0
+    fn hello_world_hash() -> [u8; 64] {
+        hex!(
             "
-            )
-        }
+            021ced8799296ceca557832ab941a50b4a11f83478cf141f51f933f653ab9fbc
+            c05a037cddbed06e309bf334942c4e58cdf1a46e237911ccd7fcf9787cbc7fd0
+        "
+        )
+    }
+
+    #[cfg(test)]
+    mod build_tree {
+        use super::*;
 
         #[test]
         fn empty_input() {
@@ -173,6 +204,61 @@ mod tests {
                     &MerkleTree::leaf64(hello_world_hash())
                 ))
             );
+        }
+    }
+
+    #[cfg(test)]
+    mod push {
+        use super::*;
+
+        #[test]
+        fn onto_leaf() {
+            let leaf = MerkleTree::leaf64(hello_world_hash());
+            let pushed = leaf.push(&b"hello world"[..]);
+
+            let from_sequence =
+                build_tree::<Blake2b, _, _>(&[&b"hello world"[..], &b"hello world"[..]]).unwrap();
+
+            assert_eq!(pushed, from_sequence);
+        }
+
+        #[test]
+        fn imbalanced_tree() {
+            let three_items = build_tree::<Blake2b, _, _>(&[
+                &b"hello world"[..],
+                &b"hello world"[..],
+                &b"hello world"[..],
+            ])
+            .unwrap();
+
+            let four_items = build_tree::<Blake2b, _, _>(&[
+                &b"hello world"[..],
+                &b"hello world"[..],
+                &b"hello world"[..],
+                &b"hello world"[..],
+            ])
+            .unwrap();
+
+            let pushed = three_items.push(&b"hello world"[..]);
+
+            assert_eq!(pushed, four_items);
+        }
+
+        #[test]
+        fn balanced_tree() {
+            let two_items =
+                build_tree::<Blake2b, _, _>(&[&b"hello world"[..], &b"hello world"[..]]).unwrap();
+
+            let three_items = build_tree::<Blake2b, _, _>(&[
+                &b"hello world"[..],
+                &b"hello world"[..],
+                &b"hello world"[..],
+            ])
+            .unwrap();
+
+            let pushed = two_items.push(&b"hello world"[..]);
+
+            assert_eq!(pushed, three_items);
         }
     }
 }
