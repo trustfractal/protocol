a9f2e3421674a69720a1afd85f2aa79990aed1dc
Boy Maas <boy.maas@gmail.com>
2021-08-10 15:43:15 +0200

    empty, new, and extend functions added and tested

--- support/merklex-js/src/lib.rs
+++ support/merklex-js/src/lib.rs
@@ -17,8 +17,21 @@ extern "C" {
     fn alert(s: &str);
 }
 
+// #[wasm_bindgen]
+// pub fn build(s: &str) -> Option<Vec<u8>> {
+//     let tree = MerkleTree::<Blake2b>::from_iter(&[s])?;
+//     Some(tree.encode())
+// }
+
 #[wasm_bindgen]
-pub fn build(s: &str) -> Option<Vec<u8>> {
-    let tree = MerkleTree::<Blake2b>::from_iter(&[s])?;
-    Some(tree.encode())
+pub fn build(s: &str) -> Option<String> {
+    MerkleTree::<Blake2b>::from_iter(&[s]).map(|v| hex::encode(v.encode()))
+}
+
+#[wasm_bindgen]
+pub fn extend(mtree: &str, s: &str) -> Option<String> {
+    hex::decode(mtree)
+        .ok()
+        .map(|b| MerkleTree::<Blake2b>::leaf_bytes(b))
+        .map(|tree| hex::encode(tree.push(s).encode()))
 }
--- support/merklex-js/tests/web.rs
+++ support/merklex-js/tests/web.rs
@@ -7,7 +7,26 @@ use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+use merklex_js;
+
+static merke_empty: &str    = "4004786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce";
+static merkle_a: &str       = "4004021ced8799296ceca557832ab941a50b4a11f83478cf141f51f933f653ab9fbcc05a037cddbed06e309bf334942c4e58cdf1a46e237911ccd7fcf9787cbc7fd0";
+static merkle_b_ext_a: &str = "4008d6f80189753931698cbde8a0ede7f4f33131c546dfa2e10ca4b69c897c4b7430fcac17dababf2db20115a5a59240cd6b465dc368356cb01170cf11ad1fe14167021ced8799296ceca557832ab941a50b4a11f83478cf141f51f933f653ab9fbcc05a037cddbed06e309bf334942c4e58cdf1a46e237911ccd7fcf9787cbc7fd0";
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn build_an_empty_merkle_tree() {
+    let r = merklex_js::build("").unwrap();
+    assert_eq!(r, merkle_a);
+}
+
+#[wasm_bindgen_test]
+fn build_a_new_merkle_tree() {
+    let r = merklex_js::build("hello world").unwrap();
+    assert_eq!(r, merkle_a);
+}
+
+#[wasm_bindgen_test]
+fn extend_a_merkle_tree() {
+    let r = merklex_js::extend(merkle_a, "hello world").unwrap();
+    assert_eq!(r, merkle_b_ext_a);
 }
