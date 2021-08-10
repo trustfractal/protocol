2e74426f43d7a74ebf36703f2d4443af3cef4685
Boy Maas <boy.maas@gmail.com>
2021-08-10 14:28:52 +0200

    merklex build added

--- support/merklex-js/src/lib.rs
+++ support/merklex-js/src/lib.rs
@@ -1,5 +1,9 @@
 mod utils;
 
+use blake2::Blake2b;
+use merklex::MerkleTree;
+use parity_scale_codec::Encode;
+
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +13,12 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, merklex-js!");
+pub fn build(s: &str) -> Option<Vec<u8>> {
+    let tree = MerkleTree::<Blake2b>::from_iter(&[s])?;
+    Some(tree.encode())
 }
