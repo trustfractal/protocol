6d14f8a446b051345240ad1cf6ae8814528186e9
Shelby Doolittle <shelby@shelbyd.com>
2021-09-03 14:31:35 -0400

    add types.json file

--- scripts/download_chain.sh
+++ scripts/download_chain.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 sub2sql --chain=wss://nodes.testnet.fractalprotocol.com \
   --out=testnet.sqlite \
-  --types='{"FractalId": "u64", "MerkleTree": "Raw"}'
+  --types="$(cat blockchain/types.json | tr -d '\n')"
