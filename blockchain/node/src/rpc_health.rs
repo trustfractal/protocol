a11331ac6d875ddc3740d29cfc7ecd5a5300a215
Boy Maas <boy.maas@gmail.com>
2021-10-02 14:41:48 +0200

    some experiments to get the rpc working

--- blockchain/node/src/rpc_health.rs
+++ blockchain/node/src/rpc_health.rs
@@ -13,7 +13,7 @@ pub struct Health;
 impl HealthApi for Health {
     fn ready(&self) -> Result<u64> {
         Err(jsonrpc_core::Error {
-            code: jsonrpc_core::ErrorCode::ServerError(400),
+            code: jsonrpc_core::ErrorCode::InternalError,
             data: None,
             message: "Node is synchronizing".to_string(),
         })
