d5ef5459395d5a6680b0afb2a9029f3d25d85f5a
Boy Maas <boy.maas@gmail.com>
2021-10-02 15:51:14 +0200

    working parsing of Health object

--- support/node-health-check/src/main.rs
+++ support/node-health-check/src/main.rs
@@ -1,10 +1,29 @@
 use warp::Filter;
 
+use anyhow::Result;
 use std::{sync::Arc, time::Duration};
 use tokio::sync::Mutex;
 
+use std::collections::HashMap;
+
 use std::path::PathBuf;
 use structopt::StructOpt;
+
+use serde::{Deserialize, Serialize};
+
+/// Health struct returned by the RPC
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Health {
+    /// Number of connected peers
+    pub peers: usize,
+    /// Is the node syncing
+    pub is_syncing: bool,
+    /// Should this node have any peers
+    ///
+    /// Might be false for local chains or when running without discovery.
+    pub should_have_peers: bool,
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -14,10 +33,10 @@ use structopt::StructOpt;
 struct Opt {
     /// Set poll_interval
     #[structopt(short = "i", default_value = "5")]
-    poll_interval: f64,
+    poll_interval: u64,
 
     /// Node rpc endpoint
-    #[structopt(short, long, env, default_value = "http://localhost:9933")]
+    #[structopt(short, long, env, default_value = "http://localhost:9933/health")]
     node_rpc_endpoint: String,
 
     // The port to listen on
@@ -25,21 +44,31 @@ struct Opt {
     port: u16,
 }
 
-async fn poller() {
+async fn poller(opts: Opt) {
     loop {
         // poll the services here
         //
+        let resp = async {
+            reqwest::get(&opts.node_rpc_endpoint)
+                .await?
+                .json::<Health>()
+                .await
+        };
 
-        tokio::time::sleep(Duration::from_secs(5)).await;
+        println!("{:#?}", resp.await);
+
+        tokio::time::sleep(Duration::from_secs(opts.poll_interval)).await;
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let opt = Opt::from_args();
+    let opts = Opt::from_args();
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let health = warp::path!("health").map(|| format!("healthy!"));
+
+    tokio::spawn(poller(opts));
 
     warp::serve(health).run(([0, 0, 0, 0], 9955)).await;
 }
