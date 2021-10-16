f2088e60b7ce9a9938350d3f254e451ae08e0e2a
Boy Maas <boy.maas@gmail.com>
2021-10-16 15:08:52 +0200

    removed dc --help

--- infra/net/packer/fcl-asg-node/provision.sh
+++ infra/net/packer/fcl-asg-node/provision.sh
@@ -22,5 +22,3 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 ## Install the docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose --help
