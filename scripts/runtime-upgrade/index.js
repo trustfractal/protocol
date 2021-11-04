324a71677362ad2ca8cc70c48d0edec14139f153
melatron <antoni.dikov@gmail.com>
2021-11-04 15:08:20 +0200

    review fix

--- scripts/runtime-upgrade/index.js
+++ scripts/runtime-upgrade/index.js
@@ -29,16 +29,21 @@ async function main() {
 
     // Retrieve the upgrade key from the chain state
     const adminId = await api.query.sudo.key();
+
+    if (`${adminId}` != newPair.address) {
+        console.error(`The provided key - ${newPair.address} doesn't match the sudo key - ${adminId}`)
+        process.exit(0);
+    }
     console.log(`Upgrading from ${adminId}, ${code.length / 2} bytes`);
 
     // Perform the actual chain upgrade via the sudo module
     const txn = TxnWatcher.signAndSend(api.tx.sudo.sudoUncheckedWeight(proposal, 1), newPair);
-    await txn.inBlock();
-    console.log('Included in block'); //TODO: include the block hash when TxnWatcher is updated to return it from `inBlock`
-    //TODO: Also we can log the events here
-    await txn.finalized();
-    console.log('Finalized'); //TODO: include the finalized block hash when TxnWatcher is updated to return it from `inBlock`
-    process.exit(1);
+    let data = await txn.inBlock();
+    console.log('Included in block'); //TODO: console.log(`Included in block - ${data.block}`);
+
+    let finalizedData = await txn.finalized();
+    console.log('Finalized'); //TODO console.log(`Finalized and included in block - ${finalizedData.includedInBlock}`);
+    process.exit(0);
 }
 
 main().catch((error) => {
--- scripts/runtime-upgrade/upgrade_chain.sh
+++ scripts/runtime-upgrade/upgrade_chain.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-(cd $SCRIPT_DIR../../ ; cargo build --release)
+ROOT_DIR="$SCRIPT_DIR/../../"
+(cd $ROOT_DIR ; cargo build --release)
 (cd $SCRIPT_DIR ; yarn)
-node $SCRIPT_DIR/index.js --nodeAddress $1 --privateKey $2 --wasmPath $SCRIPT_DIR/../../target/release/wbuild/fractal-protocol-blockchain-runtime/fractal_protocol_blockchain_runtime.wasm
+node $SCRIPT_DIR/index.js --nodeAddress $1 --privateKey $2 --wasmPath $ROOT_DIR/target/release/wbuild/fractal-protocol-blockchain-runtime/fractal_protocol_blockchain_runtime.wasm
