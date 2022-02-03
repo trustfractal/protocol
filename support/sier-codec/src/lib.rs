f464f9805cc8fe2d0d5dd85de8e0cd51812ee866
melatron <antoni.dikov@gmail.com>
2022-02-03 21:26:12 +0200

    Add integration test for json to sier

--- support/sier-codec/src/lib.rs
+++ support/sier-codec/src/lib.rs
@@ -11,7 +11,9 @@ use object::{Object, Value};
 mod schema;
 use schema::{FieldDef, Id, StructDef, Type};
 
-#[derive(Debug)]
+use serde_json::Value as SerdeValue;
+
+#[derive(Debug, Default)]
 pub struct Parser {
     structs: HashMap<Id, Arc<StructDef>>,
 }
@@ -48,13 +50,54 @@ impl Parser {
     pub fn struct_def(&self, name: &str) -> Option<&Arc<StructDef>> {
         self.structs.values().find(|s| s.type_name() == name)
     }
-}
 
-impl Default for Parser {
-    fn default() -> Self {
-        Parser {
-            structs: HashMap::new(),
+    pub fn transform_serde_value<'s>(
+        json_obj: &serde_json::Map<String, SerdeValue>,
+    ) -> (String, Vec<FieldDef>, Vec<Value<'s>>) {
+        let mut values = Vec::with_capacity(json_obj.len());
+        let mut fields = Vec::with_capacity(json_obj.len());
+        //TODO(melatron): Generate a name for the Json Struct Def so that there is no collision.
+        let mut type_name = String::from("");
+        for (key, value) in json_obj {
+            let (field_type, value) = match value {
+                SerdeValue::Null => (Type::Unit, Value::Unit),
+                SerdeValue::Bool(_bool) => (Type::Unit, Value::Unit),
+                SerdeValue::Number(number) => {
+                    if let Some(n) = number.as_u64() {
+                        (Type::U64, Value::U64(n))
+                    } else {
+                        (Type::Unit, Value::Unit)
+                    }
+                }
+                SerdeValue::String(s) => (Type::String, Value::String(s.to_string())),
+                SerdeValue::Array(_vec) => unimplemented!(), // (Type::Unit, Value::List(vec![])),
+                SerdeValue::Object(_inner_json_obj) => unimplemented!(), //(Type::Unit, Value::Unit),
+            };
+            type_name.push_str(key.as_str());
+            values.push(value);
+            fields.push(FieldDef {
+                name: key.to_string(),
+                type_: field_type,
+            })
         }
+        (type_name, fields, values)
+    }
+
+    pub fn parse_json<'i>(&mut self, file_json_contents: &'i str) -> Result<Object, Error<'i>> {
+        let json: SerdeValue =
+            serde_json::from_str(file_json_contents).or(Err(Error::InvalidJson))?;
+
+        let (type_name, fields, values) =
+            Parser::transform_serde_value(json.as_object().ok_or(Error::InvalidJson)?);
+        let json_def = StructDef { type_name, fields };
+        let rw_def = Arc::new(json_def);
+        let id = rw_def.id();
+        let existing = self.structs.insert(id, rw_def);
+        if let Some(s) = existing {
+            return Err(Error::DuplicateStructDef(s.type_name().to_string()));
+        }
+
+        Ok(Object::new(self.structs.get(&id).unwrap().as_ref(), values))
     }
 }
 
@@ -70,6 +113,7 @@ pub enum Error<'i> {
     TooFewBytes,
     TooManyBytes,
     InvalidUtf8(std::str::Utf8Error),
+    InvalidJson,
 }
 
 #[cfg(test)]
