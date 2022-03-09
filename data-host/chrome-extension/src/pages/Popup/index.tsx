f9dc4ec6c5fc7c27eb139669b01196957aa89ab8
melatron <antoni.dikov@gmail.com>
2022-03-09 13:15:36 +0200

    Remove data-host library and example chrome extension.

--- data-host/chrome-extension/src/pages/Popup/index.tsx
+++
@@ -1,7 +0,0 @@
-
-import { FractalUI } from '@trustfractal/data-host';
-import './index.css';
-
-new FractalUI().render(document.getElementById('app-container'));
-
-if (module.hot) module.hot.accept();
