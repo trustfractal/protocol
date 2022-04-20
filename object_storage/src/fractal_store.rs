9996bc9ab5d54acd72f3dc2735dfb091f38b555a
Shelby Doolittle <shelbyd@users.noreply.github.com>
2022-04-19 23:56:56 -0400

    Begin implementation of Cross Chain Object Storage (#137)
    * Simple (failing) e2e test for Object Storage
    
    * Implement FractalStore#init
    
    * Implement KvSet#insert and #contains
    
    * Implement inefficient lexicographic iteration of KvSet
    
    * Store owned reference to database in KvSet
    
    * Balance tree in KvSet
    
    * Implement MerkleTree hasher
    
    * Clippy fixes
    
    * More clippy fixes
    
    * Implement some of generating proofs
    
    * Simple object value proof working e2e
    
    * Add some documentation to KvSet methods
    
    * Clippy fixes
    
    * Review comments and remove the Given type

---
+++ object_storage/src/fractal_store.rs
@@ -0,0 +1,273 @@
+use crate::{merkle_tree::MerkleTree, *};
+
+use fallible_iterator::FallibleIterator;
+use std::ops::Deref;
+
+#[mockall_double::double]
+use crate::kv_set::KvSet;
+
+const OBJECT_DATA: &[u8] = &[1];
+const OBJECT_IDS: &[u8] = &[2];
+
+pub struct FractalStore<D: Database + 'static> {
+    db: Handle<D>,
+    objects: KvSet<D>,
+}
+
+impl<D: Database + 'static> FractalStore<D> {
+    pub fn new(db: D) -> Self {
+        let handle = Handle::new(db);
+        let kv = KvSet::new(PrefixedHandle::new(OBJECT_IDS, &handle));
+        Self::new_with_deps(handle, kv)
+    }
+
+    fn new_with_deps(db: Handle<D>, objects: KvSet<D>) -> Self {
+        FractalStore { db, objects }
+    }
+
+    /// Create a new object with the specified id and value.
+    pub fn init(&mut self, id: &[u8], value: &[u8]) -> Result<(), InitError<D::Error>> {
+        if self.objects.contains(id)? {
+            return Err(InitError::IdExists);
+        }
+
+        self.db
+            .borrow_mut()
+            .store_slices(&[OBJECT_DATA, id], value)?;
+        self.objects.insert(id)?;
+
+        Ok(())
+    }
+
+    /// Calculate the hash representing the state of the entire tree.
+    pub fn root_hash(&self) -> Result<Hash, D::Error> {
+        let mut merkle_tree = MerkleTree::new();
+
+        let mut object_ids = self.objects.iter_lexicographic();
+        while let Some(object_id) = object_ids.next()? {
+            let data = self
+                .object_data(&object_id)?
+                .expect("should not have an object id without data");
+            merkle_tree.update(object_hash(&object_id, &data));
+        }
+
+        Ok(merkle_tree.finalize())
+    }
+
+    fn object_data(&self, object_id: &[u8]) -> Result<Option<Vec<u8>>, D::Error> {
+        self.db.borrow().read_slices(&[OBJECT_DATA, object_id])
+    }
+
+    /// Generate a proof of the proposition given the provided one.
+    pub fn prove_given(
+        &self,
+        prop: Proposition,
+        given: Proposition,
+    ) -> Result<Proof, ProofError<D::Error>> {
+        match (given, prop) {
+            (Proposition::RootIs(root), Proposition::ObjectIsValue(object_id, value)) => {
+                if root != self.root_hash()? {
+                    return Err(ProofError::RootHashMismatch);
+                }
+
+                let data = self
+                    .object_data(&object_id)?
+                    .ok_or(ProofError::MissingObject)?;
+                if value != data {
+                    return Err(ProofError::ValueMismatch);
+                }
+                let needed_hash = object_hash(&object_id, &data);
+
+                self.prove_given(
+                    Proposition::HashInObjectTree(needed_hash),
+                    Proposition::RootIs(root),
+                )
+            }
+            (Proposition::RootIs(root), Proposition::HashInObjectTree(hash)) => {
+                if root == hash {
+                    return Ok(Proof::Empty);
+                }
+
+                unimplemented!("hash_in_object_tree");
+            }
+            unhandled => {
+                unimplemented!("unhandled: {:?}", unhandled);
+            }
+        }
+    }
+}
+
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
+pub enum InitError<E> {
+    Db(#[from] E),
+    IdExists,
+}
+
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
+pub enum ProofError<E> {
+    Db(#[from] E),
+    RootHashMismatch,
+    MissingObject,
+    ValueMismatch,
+}
+
+pub fn object_hash(object_id: &[u8], object_data: &[u8]) -> Hash {
+    use blake2::{Blake2b512, Digest};
+
+    let mut hasher = Blake2b512::default();
+    hasher.update(object_id);
+    hasher.update(Blake2b512::digest(object_data));
+    hasher.finalize().into()
+}
+
+pub trait Database {
+    type Error;
+
+    fn store(&mut self, key: &[u8], value: &[u8]) -> Result<(), Self::Error>;
+    fn read(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Self::Error>;
+
+    fn store_iter<'b>(
+        &mut self,
+        key: impl IntoIterator<Item = &'b u8>,
+        value: &[u8],
+    ) -> Result<(), Self::Error> {
+        self.store(key.into_iter().cloned().collect::<Vec<_>>().as_ref(), value)
+    }
+
+    fn store_slices(&mut self, key_slices: &[&[u8]], value: &[u8]) -> Result<(), Self::Error> {
+        self.store_iter(key_slices.iter().flat_map(|v| v.iter()), value)
+    }
+
+    fn read_iter<'b>(
+        &self,
+        key: impl IntoIterator<Item = &'b u8>,
+    ) -> Result<Option<Vec<u8>>, Self::Error> {
+        self.read(key.into_iter().cloned().collect::<Vec<_>>().as_ref())
+    }
+
+    fn read_slices(&self, key_slices: &[&[u8]]) -> Result<Option<Vec<u8>>, Self::Error> {
+        self.read_iter(key_slices.iter().flat_map(|v| v.iter()))
+    }
+}
+
+pub struct Handle<T>(std::rc::Rc<std::cell::RefCell<T>>);
+
+impl<T> Handle<T> {
+    pub fn new(item: T) -> Self {
+        Handle(std::rc::Rc::new(std::cell::RefCell::new(item)))
+    }
+}
+
+impl<T> Clone for Handle<T> {
+    fn clone(&self) -> Self {
+        Handle(std::rc::Rc::clone(&self.0))
+    }
+}
+
+impl<T> Deref for Handle<T> {
+    type Target = std::cell::RefCell<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct PrefixedHandle<D> {
+    prefix: Vec<u8>,
+    handle: Handle<D>,
+}
+
+impl<D> PrefixedHandle<D> {
+    pub fn new(prefix: &[u8], handle: &Handle<D>) -> Self {
+        PrefixedHandle {
+            prefix: prefix.to_vec(),
+            handle: handle.clone(),
+        }
+    }
+}
+
+impl<D> Clone for PrefixedHandle<D> {
+    fn clone(&self) -> Self {
+        PrefixedHandle {
+            prefix: self.prefix.clone(),
+            handle: self.handle.clone(),
+        }
+    }
+}
+
+impl<D: Database> Database for PrefixedHandle<D> {
+    type Error = D::Error;
+
+    fn store(&mut self, key: &[u8], value: &[u8]) -> Result<(), D::Error> {
+        self.handle
+            .borrow_mut()
+            .store_slices(&[&self.prefix, key], value)
+    }
+
+    fn read(&self, key: &[u8]) -> Result<Option<Vec<u8>>, D::Error> {
+        self.handle.borrow().read_slices(&[&self.prefix, key])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::{kv_set::MockKvSet, test::InMemoryDb};
+    use mockall::predicate::*;
+
+    fn mock_kv_set(expectations: impl FnOnce(&mut MockKvSet<InMemoryDb>)) -> MockKvSet<InMemoryDb> {
+        let mut kv_set = MockKvSet::default();
+        expectations(&mut kv_set);
+        kv_set.expect_insert().return_const(Ok(()));
+        kv_set.expect_contains().return_const(Ok(false));
+        kv_set
+    }
+
+    #[cfg(test)]
+    mod init {
+        use super::*;
+
+        #[test]
+        fn stores_bytes_in_value() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut store = FractalStore::new_with_deps(db.clone(), mock_kv_set(|_| {}));
+
+            store.init(&[42], b"foo").unwrap();
+
+            assert_eq!(db.borrow().read(&[1, 42]).unwrap(), Some(b"foo".to_vec()));
+        }
+
+        #[test]
+        fn adds_item_id_to_kv_storage_set() {
+            let kv_set = mock_kv_set(|kv_set| {
+                kv_set
+                    .expect_insert()
+                    .return_const(Ok(()))
+                    .with(eq(&[42][..]))
+                    .once();
+            });
+
+            let db = Handle::new(InMemoryDb::default());
+            let mut store = FractalStore::new_with_deps(db.clone(), kv_set);
+
+            store.init(&[42], b"foo").unwrap();
+        }
+
+        #[test]
+        fn fails_if_id_exists() {
+            let kv_set = mock_kv_set(|kv_set| {
+                kv_set
+                    .expect_contains()
+                    .with(eq(&[42][..]))
+                    .return_const(Ok(true));
+            });
+
+            let db = Handle::new(InMemoryDb::default());
+            let mut store = FractalStore::new_with_deps(db.clone(), kv_set);
+
+            assert_eq!(store.init(&[42], b"foo"), Err(InitError::IdExists));
+            assert_eq!(db.borrow().read(&[1, 42]).unwrap(), None);
+        }
+    }
+}
---
+++ object_storage/src/kv_set.rs
@@ -0,0 +1,450 @@
+use crate::*;
+
+use fallible_iterator::FallibleIterator;
+use parity_scale_codec::{Compact, Decode, Encode};
+use std::cmp::Ordering;
+
+/// Set of byte-strings backed by a AVL tree in a generic key-value database.
+///
+/// Allows for in-order traversal with log(n) insert, delete, and contains.
+pub(crate) struct KvSet<D> {
+    handle: PrefixedHandle<D>,
+}
+
+// TODO(shelbyd): Reduce number of reads / writes.
+#[cfg_attr(test, mockall::automock, allow(dead_code))]
+impl<D: Database + 'static> KvSet<D> {
+    pub fn new(handle: PrefixedHandle<D>) -> Self {
+        KvSet { handle }
+    }
+
+    /// Inserts the provided byte-string into the set.
+    ///
+    /// If the value already exists, this function does nothing.
+    pub fn insert(&mut self, key: &[u8]) -> Result<(), D::Error> {
+        self.insert_at(0, key)?;
+        Ok(())
+    }
+
+    fn get_next_index(&mut self) -> Result<u64, D::Error> {
+        let read = self.handle.read(&[])?;
+        let index = match read {
+            Some(bytes) => <Compact<u64> as Decode>::decode(&mut &bytes[..]).unwrap().0,
+            None => 1,
+        };
+        self.handle.store(&[], &Compact(index + 1).encode())?;
+        Ok(index)
+    }
+
+    fn insert_at(&mut self, index: u64, key: &[u8]) -> Result<bool, D::Error> {
+        let did_insert = self.do_insert(index, key)?;
+        if did_insert {
+            self.rebalance(index)?;
+        }
+        Ok(did_insert)
+    }
+
+    fn do_insert(&mut self, index: u64, key: &[u8]) -> Result<bool, D::Error> {
+        let make_node = || Node {
+            left: None,
+            right: None,
+            height: 1,
+            data: key.to_vec(),
+        };
+
+        let read = self.get_node(index)?;
+        let mut node = match read {
+            None => {
+                self.set_node(index, make_node())?;
+                return Ok(true);
+            }
+            Some(node) => node,
+        };
+        let dest = match lexicographic_compare(key, &node.data) {
+            Ordering::Less => &mut node.left,
+            Ordering::Greater => &mut node.right,
+            Ordering::Equal => return Ok(false),
+        };
+        if let Some(i) = dest {
+            let inserted = self.insert_at(*i, key)?;
+            self.set_node(index, node)?;
+            return Ok(inserted);
+        }
+
+        let new_index = self.get_next_index()?;
+        *dest = Some(new_index);
+
+        self.set_node(new_index, make_node())?;
+        self.set_node(index, node)?;
+
+        Ok(true)
+    }
+
+    fn set_node(&mut self, index: u64, mut node: Node) -> Result<(), D::Error> {
+        let left_height = match node.left {
+            None => 0,
+            Some(l) => self.get_node(l)?.unwrap().height,
+        };
+        let right_height = match node.right {
+            None => 0,
+            Some(r) => self.get_node(r)?.unwrap().height,
+        };
+
+        node.height = core::cmp::max(left_height, right_height) + 1;
+        self.handle.store(&Compact(index).encode(), &node.encode())
+    }
+
+    fn rebalance(&mut self, index: u64) -> Result<(), D::Error> {
+        let this = self.get_node(index)?.unwrap();
+
+        match (this.left, this.right) {
+            (None, None) => Ok(()),
+            (None, Some(i)) => {
+                let right = self.get_node(i)?.unwrap();
+                if right.height == 0 {
+                    self.set_node(index, this)?;
+                    return Ok(());
+                }
+
+                self.rotate_right(i)?;
+                self.rotate_left(index)?;
+                Ok(())
+            }
+            (Some(i), None) => {
+                let left = self.get_node(i)?.unwrap();
+                if left.height == 0 {
+                    self.set_node(index, this)?;
+                    return Ok(());
+                }
+
+                self.rotate_left(i)?;
+                self.rotate_right(index)?;
+                Ok(())
+            }
+            (Some(l), Some(r)) => {
+                let left = self.get_node(l)?.unwrap();
+                let right = self.get_node(r)?.unwrap();
+
+                match right.height.wrapping_sub(left.height) {
+                    255 | 0 | 1 => Ok(()),
+                    2 => self.rotate_left(index),
+                    254 => self.rotate_right(index),
+                    i => unreachable!("{:?}", i),
+                }
+            }
+        }
+    }
+
+    fn rotate_right(&mut self, index: u64) -> Result<(), D::Error> {
+        let mut this = self.get_node(index)?.unwrap();
+        let (l_index, mut left) = match this.left {
+            None => return Ok(()),
+            Some(l) => (l, self.get_node(l)?.unwrap()),
+        };
+        // This and left swap indices.
+        this.left = left.right;
+        left.right = Some(l_index);
+
+        self.set_node(l_index, this)?;
+        self.set_node(index, left)?;
+
+        Ok(())
+    }
+
+    fn rotate_left(&mut self, index: u64) -> Result<(), D::Error> {
+        let mut this = self.get_node(index)?.unwrap();
+        let (r_index, mut right) = match this.right {
+            None => return Ok(()),
+            Some(r) => (r, self.get_node(r)?.unwrap()),
+        };
+        // This and left swap indices.
+        this.right = right.left;
+        right.left = Some(r_index);
+
+        self.set_node(r_index, this)?;
+        self.set_node(index, right)?;
+
+        Ok(())
+    }
+
+    /// Does the set contain the specified byte-string?
+    pub fn contains(&self, key: &[u8]) -> Result<bool, D::Error> {
+        let node = self.get_node(0)?;
+        Ok(match node {
+            None => false,
+            Some(n) => n.data == key,
+        })
+    }
+
+    fn get_node(&self, index: u64) -> Result<Option<Node>, D::Error> {
+        Ok(self
+            .handle
+            .read(&Compact(index).encode())?
+            .map(Node::decode))
+    }
+
+    /// Iterate over the items in the set in lexicographic order.
+    pub fn iter_lexicographic(&self) -> impl FallibleIterator<Item = Vec<u8>, Error = D::Error> {
+        let handle = self.handle.clone();
+
+        enum StackItem {
+            AlreadyRead(Vec<u8>),
+            DoRead(u64),
+        }
+
+        let mut stack = vec![StackItem::DoRead(0)];
+
+        fallible_iterator::convert(std::iter::from_fn(move || loop {
+            let index = match stack.pop()? {
+                StackItem::AlreadyRead(bytes) => return Some(Ok(bytes)),
+                StackItem::DoRead(index) => index,
+            };
+
+            let bytes = match handle.read(&Compact(index).encode()) {
+                Err(e) => return Some(Err(e)),
+                Ok(v) => v,
+            };
+
+            if let Some(key) = bytes {
+                let node = Node::decode(key);
+
+                // Actual operations are performed in reverse of this order.
+                if let Some(r) = node.right {
+                    stack.push(StackItem::DoRead(r));
+                }
+                stack.push(StackItem::AlreadyRead(node.data));
+                if let Some(l) = node.left {
+                    stack.push(StackItem::DoRead(l));
+                }
+            }
+        }))
+    }
+
+    #[cfg(test)]
+    fn height(&self, index: u64) -> Result<u8, D::Error> {
+        let node = match self.get_node(index)? {
+            None => return Ok(0),
+            Some(n) => n,
+        };
+
+        Ok(1 + match (node.left, node.right) {
+            (None, None) => 0,
+            (Some(n), None) | (None, Some(n)) => self.height(n)?,
+            (Some(l), Some(r)) => core::cmp::max(self.height(l)?, self.height(r)?),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    left: Option<u64>,
+    right: Option<u64>,
+    height: u8,
+    data: Vec<u8>,
+}
+
+impl Node {
+    fn encode(&self) -> Vec<u8> {
+        let mut out = Vec::new();
+        self.left.map(Compact).encode_to(&mut out);
+        self.right.map(Compact).encode_to(&mut out);
+        out.push(self.height);
+        out.extend(&self.data);
+        out
+    }
+
+    fn decode(mut bytes: Vec<u8>) -> Self {
+        let mut slice = &bytes[..];
+        let left = <Option<Compact<u64>> as Decode>::decode(&mut slice)
+            .unwrap()
+            .map(|c| c.0);
+        let right = <Option<Compact<u64>> as Decode>::decode(&mut slice)
+            .unwrap()
+            .map(|c| c.0);
+        let (&height, slice) = slice.split_first().unwrap();
+
+        let index = bytes.len() - slice.len();
+        let data = bytes.split_off(index);
+        Node {
+            left,
+            right,
+            height,
+            data,
+        }
+    }
+}
+
+fn lexicographic_compare(mut a: &[u8], mut b: &[u8]) -> Ordering {
+    loop {
+        return match (a.get(0), b.get(0)) {
+            (None, None) => Ordering::Equal,
+            (None, _) => Ordering::Less,
+            (_, None) => Ordering::Greater,
+            (Some(a0), Some(b0)) => match a0.cmp(b0) {
+                Ordering::Equal => {
+                    a = &a[1..];
+                    b = &b[1..];
+                    continue;
+                }
+                o => o,
+            },
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::test::*;
+
+    #[test]
+    fn new_contains_nothing() {
+        let db = Handle::new(InMemoryDb::default());
+        let kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+        assert_eq!(kv_set.contains(b"foo"), Ok(false));
+    }
+
+    #[test]
+    fn insert_contains_key() {
+        let db = Handle::new(InMemoryDb::default());
+        let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+        kv_set.insert(b"foo").unwrap();
+
+        assert_eq!(kv_set.contains(b"foo"), Ok(true));
+    }
+
+    #[test]
+    fn prefixes_db_entries_with_provided_prefix() {
+        let db = Handle::new(InMemoryDb::default());
+        let prefix = &[1, 2, 3];
+        let mut kv_set = KvSet::new(PrefixedHandle::new(prefix, &db));
+
+        kv_set.insert(b"foo").unwrap();
+
+        for key in db.borrow().keys() {
+            assert_eq!(&key[..prefix.len()], prefix);
+        }
+    }
+
+    #[cfg(test)]
+    mod iter_lexicographic {
+        use super::*;
+
+        #[test]
+        fn empty_is_empty_iter() {
+            let db = Handle::new(InMemoryDb::default());
+            let kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            assert_eq!(kv_set.iter_lexicographic().next(), Ok(None));
+        }
+
+        #[test]
+        fn contains_a_single_value() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            kv_set.insert(&[42]).unwrap();
+
+            let mut iter = kv_set.iter_lexicographic();
+            assert_eq!(iter.next(), Ok(Some(vec![42])));
+            assert_eq!(iter.next(), Ok(None));
+        }
+
+        #[test]
+        fn contains_multiple_values() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            kv_set.insert(&[42]).unwrap();
+            kv_set.insert(&[43]).unwrap();
+            kv_set.insert(&[44]).unwrap();
+
+            let mut iter = kv_set.iter_lexicographic();
+            assert_eq!(iter.next(), Ok(Some(vec![42])));
+            assert_eq!(iter.next(), Ok(Some(vec![43])));
+            assert_eq!(iter.next(), Ok(Some(vec![44])));
+            assert_eq!(iter.next(), Ok(None));
+        }
+
+        #[test]
+        fn returns_values_in_lexicographic_order() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            kv_set.insert(&[44]).unwrap();
+            kv_set.insert(&[43]).unwrap();
+            kv_set.insert(&[42]).unwrap();
+
+            let mut iter = kv_set.iter_lexicographic();
+            assert_eq!(iter.next(), Ok(Some(vec![42])));
+            assert_eq!(iter.next(), Ok(Some(vec![43])));
+            assert_eq!(iter.next(), Ok(Some(vec![44])));
+            assert_eq!(iter.next(), Ok(None));
+        }
+    }
+
+    #[cfg(test)]
+    mod rebalancing {
+        use super::*;
+
+        #[test]
+        fn simple() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            assert_eq!(kv_set.height(0).unwrap(), 0);
+
+            kv_set.insert(&[42]).unwrap();
+            assert_eq!(kv_set.height(0).unwrap(), 1);
+
+            kv_set.insert(&[44]).unwrap();
+            kv_set.insert(&[43]).unwrap();
+
+            assert_eq!(kv_set.height(0).unwrap(), 2);
+        }
+
+        #[test]
+        fn left_heavy() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            for i in (0..255).rev() {
+                kv_set.insert(&[i]).unwrap();
+            }
+
+            assert_eq!(kv_set.height(0).unwrap(), 8);
+        }
+
+        #[test]
+        fn right_heavy() {
+            let db = Handle::new(InMemoryDb::default());
+            let mut kv_set = KvSet::new(PrefixedHandle::new(&[1, 2, 3], &db));
+
+            for i in 0..255 {
+                kv_set.insert(&[i]).unwrap();
+            }
+
+            assert_eq!(kv_set.height(0).unwrap(), 8);
+        }
+
+        // TODO(shelbyd): Quickcheck tests for balancing.
+    }
+
+    #[test]
+    fn lexicographic_compare_test() {
+        assert_eq!(lexicographic_compare(&[], &[]), Ordering::Equal);
+        assert_eq!(lexicographic_compare(&[0], &[]), Ordering::Greater);
+        assert_eq!(lexicographic_compare(&[], &[0]), Ordering::Less);
+
+        assert_eq!(lexicographic_compare(&[0], &[0]), Ordering::Equal);
+        assert_eq!(lexicographic_compare(&[1], &[0]), Ordering::Greater);
+        assert_eq!(lexicographic_compare(&[0], &[1]), Ordering::Less);
+
+        assert_eq!(lexicographic_compare(&[0, 0], &[0, 0]), Ordering::Equal);
+        assert_eq!(lexicographic_compare(&[0, 1], &[0, 0]), Ordering::Greater);
+        assert_eq!(lexicographic_compare(&[0, 0], &[0, 1]), Ordering::Less);
+    }
+}
