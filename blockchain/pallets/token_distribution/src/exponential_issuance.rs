dddaf7fc89bf82e32e84dcff7e0c144e8b7dc0e8
Shelby Doolittle <shelby@shelbyd.com>
2022-04-20 19:10:36 -0400

    Create a pallet to manage and distribute tokens

---
+++ blockchain/pallets/token_distribution/src/exponential_issuance.rs
@@ -0,0 +1,51 @@
+use core::f64::consts::LN_2;
+use num_traits::{cast, zero};
+
+/// Exponential issuance curve in unit-less steps with a linear portion to
+/// complete issuance at step `complete_at`.
+#[derive(Debug, Clone)]
+pub struct Issuance<Balance = u128, Step = u64> {
+    pub total: Balance,
+    pub half_life: Step,
+    pub complete_at: Step,
+}
+
+impl<Balance, Step> Issuance<Balance, Step>
+where
+    Step: num_traits::PrimInt,
+    Balance: num_traits::PrimInt,
+{
+    pub fn total_issued_by(&self, index: Step) -> Balance {
+        self.total - self.unissued_at(index)
+    }
+
+    fn unissued_at(&self, index: Step) -> Balance {
+        if index >= self.complete_at {
+            return zero();
+        }
+        if index == zero() {
+            return self.total;
+        }
+
+        let total: f64 = cast(self.total).unwrap();
+        cast(self.percent_unissued_at(index) * total).unwrap()
+    }
+
+    // Exponential curve with a half-life of h = e^(-x * ln2 / h).
+    // Linear portion is tangent to the curve such that the line equals zero at
+    // `complete_at`.
+    fn percent_unissued_at(&self, index: Step) -> f64 {
+        let complete_at: f64 = cast(self.complete_at).unwrap();
+        let half_life: f64 = cast(self.half_life).unwrap();
+        let index: f64 = cast(index).unwrap();
+
+        let linear_after = complete_at - half_life / LN_2;
+
+        let e = |x: f64| libm::exp(-x * LN_2 / half_life);
+        if index > linear_after {
+            e(linear_after) - LN_2 / half_life * e(linear_after) * (index - linear_after)
+        } else {
+            e(index)
+        }
+    }
+}
---
+++ blockchain/pallets/token_distribution/src/lib.rs
@@ -0,0 +1,172 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub mod exponential_issuance;
+pub use exponential_issuance::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use codec::{Decode, Encode};
+
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    use frame_support::traits::{Currency, Get};
+
+    pub type FractalId = u64;
+
+    type BalanceOf<T> =
+        <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+        type Currency: Currency<Self::AccountId>;
+
+        type TotalIssuance: Get<BalanceOf<Self>>;
+        type IssuanceHalfLife: Get<Self::BlockNumber>;
+        type IssuanceCompleteAt: Get<Self::BlockNumber>;
+    }
+
+    #[pallet::storage]
+    pub type TotalIssuanceOffset<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
+
+    #[pallet::storage]
+    pub type DestinationWeights<T: Config> =
+        StorageMap<_, Blake2_128Concat, Destination<T::AccountId>, u32, ValueQuery>;
+
+    #[pallet::storage]
+    pub type PurposeBalances<T: Config> =
+        StorageMap<_, Blake2_128Concat, u8, BalanceOf<T>, ValueQuery>;
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::event]
+    #[pallet::metadata(BalanceOf<T> = "Balance")]
+    pub enum Event<T: Config> {}
+
+    #[pallet::error]
+    pub enum Error<T> {}
+
+    #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+    pub enum Destination<A> {
+        Address(A),
+        Purpose(u8),
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight((
+            10_000 + T::DbWeight::get().reads_writes(0, 1),
+            DispatchClass::Normal,
+            Pays::No
+        ))]
+        pub fn set_weight(
+            origin: OriginFor<T>,
+            address: Destination<T::AccountId>,
+            #[pallet::compact] weight: u32,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+            DestinationWeights::<T>::insert(address, weight);
+            Ok(())
+        }
+
+        #[pallet::weight((
+            10_000 + T::DbWeight::get().reads_writes(1, 1),
+            DispatchClass::Normal,
+            Pays::No
+        ))]
+        pub fn increment_issuance_offset(
+            origin: OriginFor<T>,
+            #[pallet::compact] amount: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+            TotalIssuanceOffset::<T>::set(TotalIssuanceOffset::<T>::get() + amount);
+            Ok(())
+        }
+
+        #[pallet::weight((
+            10_000 + T::DbWeight::get().reads_writes(1, 1),
+            DispatchClass::Normal,
+            Pays::No
+        ))]
+        pub fn mint(
+            origin: OriginFor<T>,
+            address: T::AccountId,
+            #[pallet::compact] amount: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+
+            T::Currency::deposit_creating(&address, amount);
+            TotalIssuanceOffset::<T>::set(TotalIssuanceOffset::<T>::get() + amount);
+
+            Ok(())
+        }
+    }
+
+    impl<T: Config> Pallet<T> {
+        pub fn take_from(purpose: u8) -> BalanceOf<T> {
+            PurposeBalances::<T>::take(purpose)
+        }
+
+        pub fn return_to(purpose: u8, amount: BalanceOf<T>) {
+            PurposeBalances::<T>::insert(purpose, amount);
+        }
+    }
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
+    where
+        BalanceOf<T>: num_traits::PrimInt + core::iter::Sum,
+        BlockNumberFor<T>: num_traits::PrimInt,
+    {
+        fn on_finalize(block_number: BlockNumberFor<T>) {
+            let total_weight: u32 = DestinationWeights::<T>::iter_values().sum();
+            if total_weight == 0 {
+                return;
+            }
+
+            let already_issued = T::Currency::total_issuance()
+                + PurposeBalances::<T>::iter_values().sum()
+                - TotalIssuanceOffset::<T>::get();
+
+            // Using Issuance like this makes it _technically_ possible for
+            // consensus to fail if the CPU's floating point calculations are
+            // different.
+            //
+            // If this becomes a problem, we can have this value derived from
+            // an extrinsic that an authoritative account sets. Similar to how
+            // the timestamp pallet works.
+            let issuance = crate::Issuance {
+                total: T::TotalIssuance::get(),
+                half_life: T::IssuanceHalfLife::get(),
+                complete_at: T::IssuanceCompleteAt::get(),
+            };
+            let to_issue = issuance.total_issued_by(block_number) - already_issued;
+            let unit_balance = to_issue / total_weight.into();
+
+            for (dest, weight) in DestinationWeights::<T>::iter() {
+                let to_this = unit_balance * weight.into();
+                match dest {
+                    Destination::Address(a) => {
+                        T::Currency::deposit_creating(&a, to_this);
+                    }
+                    Destination::Purpose(p) => {
+                        let new_balance = PurposeBalances::<T>::get(&p) + to_this;
+                        PurposeBalances::<T>::insert(p, new_balance);
+                    }
+                }
+            }
+        }
+    }
+}
