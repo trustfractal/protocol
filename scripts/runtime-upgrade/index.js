bc6cd1e7ec55e1a57eb7e05103e06ecb8c2f9290
Shelby Doolittle <shelby@shelbyd.com>
2022-06-28 16:33:03 -0400

    Increment chain version and refactor upgrade script.
    Upgrade script doesn't work with an "invalid character" error.
    But we can use the Polkadot UI to upgrade the chain.

--- scripts/runtime-upgrade/index.js
+++ scripts/runtime-upgrade/index.js
@@ -1,52 +1,62 @@
-const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
-const { TxnWatcher } = require('@trustfractal/polkadot-utils')
+const {ApiPromise, WsProvider, Keyring} = require('@polkadot/api');
+const {TxnWatcher} = require('@trustfractal/polkadot-utils')
 
 var argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 
 // Create a promise API instance of the passed in node address.
 async function createPromiseApi(nodeAddress) {
-    const wsProvider = new WsProvider(nodeAddress);
-    const api = await new ApiPromise({ provider: wsProvider });
-    await api.isReady;
-    return api;
+  const wsProvider = new WsProvider(nodeAddress);
+  const api = await new ApiPromise({provider : wsProvider});
+  await api.isReady;
+  return api;
 }
 
 async function main() {
-    const nodeAddress = argv.nodeAddress;
-    const api = await createPromiseApi(nodeAddress);
-    const keyring = new Keyring({ type: 'sr25519' });
+  const nodeAddress = getRequiredArg('nodeAddress');
+  const api = await createPromiseApi(nodeAddress);
+  const keyring = new Keyring({type : 'sr25519'});
 
-    // Some mnemonic phrase
-    const PHRASE = fs.readFileSync(argv.privateKey).toString().trimEnd();
+  // Some mnemonic phrase
+  const PHRASE =
+      fs.readFileSync(getRequiredArg('privateKey')).toString().trimEnd();
 
-    // Add an account, straight mnemonic
-    const newPair = keyring.addFromUri(PHRASE);
+  // Add an account, straight mnemonic
+  const sudoPair = keyring.addFromUri(PHRASE);
 
-    // Retrieve the runtime to upgrade
-    const code = fs.readFileSync(argv.wasmPath).toString('hex');
-    const proposal = api.tx.system.setCode(`0x${code}`);
+  // Retrieve the upgrade key from the chain state
+  const adminId = await api.query.sudo.key();
+  if (adminId.toString() != sudoPair.address) {
+    throw new Error(`The provided key: ${
+        sudoPair.address}, doesn't match the sudo key: ${adminId}`);
+  }
 
-    // Retrieve the upgrade key from the chain state
-    const adminId = await api.query.sudo.key();
+  // Retrieve the runtime to upgrade
+  const code = fs.readFileSync(getRequiredArg('wasmFile')).toString('hex');
+  const proposal = api.tx.system.setCode(`0x${code}`);
 
-    if (`${adminId}` != newPair.address) {
-        console.error(`The provided key - ${newPair.address} doesn't match the sudo key - ${adminId}`)
-        process.exit(0);
-    }
-    console.log(`Upgrading from ${adminId}, ${code.length / 2} bytes`);
+  console.log(`Upgrading using ${adminId}; ${code.length / 2} bytes`);
 
-    // Perform the actual chain upgrade via the sudo module
-    const txn = TxnWatcher.signAndSend(api.tx.sudo.sudoUncheckedWeight(proposal, 1), newPair);
-    let data = await txn.inBlock();
-    console.log('Included in block'); //TODO: console.log(`Included in block - ${data.block}`);
+  // Perform the actual chain upgrade via the sudo module
+  const rawTxn = api.tx.sudo.sudoUncheckedWeight(proposal, 1);
+  const txn = TxnWatcher.signAndSend(rawTxn, sudoPair);
 
-    let finalizedData = await txn.finalized();
-    console.log('Finalized'); //TODO console.log(`Finalized and included in block - ${finalizedData.includedInBlock}`);
-    process.exit(0);
+  const inBlock = await txn.inBlock();
+  console.log(`Included in block: ${inBlock.block}`);
+
+  const finalized = await txn.finalized();
+  console.log(`Finalized and included in block: ${finalized.includedInBlock}`);
 }
 
-main().catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+function getRequiredArg(name) {
+  if (argv[name] != null) {
+    return argv[name];
+  }
+
+  throw new Error(`Missing required argument '${name}'`);
+}
+
+main().then(() => { process.exit(0); }).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
