499f24b00cbabc1093d3b2110bbe96b30329c331
Shelby Doolittle <shelby@shelbyd.com>
2022-08-16 10:20:16 -0400

    Serve JS from static directory

--- explorer/src/bin/web.rs
+++ explorer/src/bin/web.rs
@@ -23,9 +23,8 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let pool = block_pool::Pool::new(
-            std::iter::repeat(())
+            (0..10)
                 .map(|_| fractal_explorer::postgres::connect(&options.postgres))
-                .take(10)
                 .collect::<anyhow::Result<Vec<_>>>()
                 .unwrap(),
         );
@@ -44,7 +43,8 @@ async fn main() -> std::io::Result<()> {
             .into_iter()
             .fold(app, |a, resource| a.service(resource));
 
-        app.service(indexing::id_to_entity::redirect_id)
+        app.service(actix_files::Files::new("/static", "./explorer/static"))
+            .service(indexing::id_to_entity::redirect_id)
             .service(web::resource("*").route(web::get().to(pages::not_found)))
     })
     .bind(("0.0.0.0", port))?
